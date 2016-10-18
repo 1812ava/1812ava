ffbedb3aa38e567f59df4e612b4722674f21e93c
1812ava <1812ava@gmail.com>
2016-10-18 14:42:21 +0800

    Add files via upload

---
+++ week5.ino
@@ -0,0 +1,23 @@
+const int ledCount = 10;
+int ledPins[]={2,3,4,5,6,7,8,9,10,11};
+void setup() {
+  // put your setup code here, to run once:
+  for(int thisLed = 0 ;thisLed < ledCount;thisLed++)
+  {
+    pinMode(ledPins[thisLed],OUTPUT);
+  }
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+int sensorValue=analogRead(A0);
+int ledLevel = map(sensorValue,750,950,0,ledCount);
+for(int thisLed = 0 ;thisLed < ledCount;thisLed++)
+  {
+    if(thisLed<ledLevel){
+      digitalWrite(ledPins[thisLed],HIGH);
+    }
+    else{
+      digitalWrite(ledPins[thisLed],LOW);
+    }
+  }
+}
